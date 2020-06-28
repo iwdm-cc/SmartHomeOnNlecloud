b4a57837ed00b1f33c45b26c4180d4a33aac406b
once <1679167928@qq.com>
2020-06-28 09:30:10 +0800

    对于ViewModel模式的，集成的ViewModel对象必须是public

--- app/src/main/java/com/zcc/smarthome/fragment/ViewMode.java
+++ app/src/main/java/com/zcc/smarthome/fragment/ViewMode.java
@@ -2,6 +2,7 @@ package com.zcc.smarthome.fragment;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
 
 import com.alibaba.fastjson.JSONArray;
 
@@ -9,7 +10,8 @@ import com.alibaba.fastjson.JSONArray;
  * 数据模型
  * */
 
-class ViewMode extends ViewModel {
+//对于ViewModel模式的，集成的ViewModel对象必须是public
+public class ViewMode extends ViewModel {
     private MutableLiveData<JSONArray> jsonArrayMutableLiveData;
     private MutableLiveData<JSONArray> homejsonArrayMutableLiveData;
 
